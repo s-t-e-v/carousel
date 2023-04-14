f0fe0e9de89b89a5a47d8eaffbdcfcf3a7b9231d
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-04-14 15:16:51 +0200

    carousel button + style image stabilization

--- src/App.js
+++ src/App.js
@@ -1,37 +1,68 @@
+import { useState } from 'react';
 
-function Arrowleft() {
+const LEFT = "LEFT",
+      RIGHT = "RIGHT";
+
+function Arrowleft({onLeftClick}) {
   return (
-    <button className="arrow arrowleft"></button>
+    <button className="arrow arrowleft" onClick={onLeftClick}></button>
   );
 }
 
-function Arrowright() {
+function Arrowright({onRightClick}) {
   return (
-    <button className="arrow arrowright"></button>
+    <button className="arrow arrowright" onClick={onRightClick}></button>
   );
 }
 
 function Carousel() {
+  const [currentImg, setCurrentImg] = useState(0);
+
+  const images = [
+    "mountain.jpg",
+    "desert.jpg",
+    "moon.jpg",
+    "waterfall.jpg",
+  ];
+
+  function handleClick(img, side) {
+
+    switch(side) {
+      case LEFT:
+        if(img > 0)
+          img--;
+        else
+          img = images.length - 1;
+        break;
+      case RIGHT:
+        if(img < images.length - 1)
+          img++;
+        else
+          img = 0;
+        break;
+      default:
+        break;
+    }
+
+    setCurrentImg(img);
+  }
+
   return (
-    <>
-      <div className="carousel">
-        <Arrowleft/>
-          <div className="slide">
-            <img src={require("./img/mountain.jpg")} alt=""/>
-          </div>
-        <Arrowright/>
-      </div>
-    </>
+    <div className="carousel">
+      <Arrowleft onLeftClick={() => handleClick(currentImg, LEFT)}/>
+        <div className="slide">
+          <img src={require("./img/" + images[currentImg])} alt=""/>
+        </div>
+      <Arrowright onRightClick={() => handleClick(currentImg, RIGHT)}/>
+    </div>
   );
 }
 
 export default function App() {
   return (
-    <>
-      <div className="app">
-        <h1>Project 1: Carousel</h1>
-        <Carousel />
-      </div>
-    </>
+    <div className="app">
+      <h1>Project 1: Carousel</h1>
+      <Carousel />
+    </div>
   );
 }
