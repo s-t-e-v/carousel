fc3dacbda091bced70f06149c9711f2c6790a33b
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-04-14 11:17:19 +0200

    add model + arrow in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+
+function Arrowleft() {
+  return (
+    <button className="arrow arrowleft">
+      arrowleft
+    </button>
+  );
+}
+
+function Arrowright() {
+  return (
+    <button className="arrow arrowright">
+      arrowright
+    </button>
+  );
+}
+
+function Carousel() {
+  return (
+    <>
+      <Arrowleft/>
+      <div className="carousel">
+        carousel
+      </div>
+      <Arrowright/>
+    </>
+  );
+}
+
 export default function App() {
   return (
     <>
       <h1>Hello World!</h1>
+      <Carousel />
     </>
   );
 }
