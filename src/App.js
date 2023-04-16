0dd8db711d3d569420271e633d8fa4cc496821a6
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-04-16 02:26:54 +0200

    remove arrow component

--- src/App.js
+++ src/App.js
@@ -6,42 +6,28 @@ const images = [
   "moon.jpg",
   "waterfall.jpg",
 ];
-      
-
-function Arrowleft({onLeftClick}) {
-  return (
-    <button className="arrow arrowleft" onClick={onLeftClick}></button>
-  );
-}
-
-function Arrowright({onRightClick}) {
-  return (
-    <button className="arrow arrowright" onClick={onRightClick}></button>
-  );
-}
-
 
 export default function App() {
 
   const [currentImg, setCurrentImg] = useState(0);
 
-  function prevSlide() {
-    setCurrentImg(currentImg === 0? images.length - 1 : currentImg - 1);
-  }
-
   function nextSlide() {
     setCurrentImg(currentImg === images.length - 1? 0 : currentImg + 1);
+  }
+
+  function prevSlide() {
+    setCurrentImg(currentImg === 0? images.length - 1 : currentImg - 1);
   }
 
   return (
     <div className="app">
       <h1>Project 1: Carousel</h1>
         <div className="carousel">
-          <Arrowleft onLeftClick={prevSlide}/>
+          <button className="arrow arrowleft" onClick={prevSlide}></button>
           <div className="slide">
             <img src={require("./img/" + images[currentImg])} alt=""/>
           </div>
-          <Arrowright onRightClick={nextSlide}/>
+          <button className="arrow arrowright" onClick={nextSlide}></button>
       </div>
     </div>
   );
